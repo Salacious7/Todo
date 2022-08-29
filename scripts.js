45e7fb2f6b261774f5231e1d3ccab3e5603d4c6e
Salacious7 <correa.aric@yahoo.com>
2022-08-29 13:48:05 -0700

    shit kinda workin

--- scripts.js
+++ scripts.js
@@ -1,47 +1,56 @@
+let buttonId = document.getElementById('create-task-button')
+let listId = document.getElementById('task-list')
+let newListText = document.getElementById('create-task-text')
+let todos = JSON.parse(localStorage.getItem("todos"))
+let todosArr = []
 
+if (localStorage.getItem('todos')) {
+    todosArr = JSON.parse(localStorage.getItem('todos'));
+}
+
+buttonId.addEventListener('click', OnClickTaskButton)
+newListText.addEventListener('keypress', function(e)
+{
+    if (e.key === "Enter") 
+        OnClickTaskButton
+})
 
 function OnClickTaskButton()
 {
-    let newListText = document.getElementById('create-task-text')
-
     if(newListText.value == '')
         return
 
     newListText.value = newListText.value.charAt(0).toUpperCase() + newListText.value.slice(1)
-        
 
     let newCheckbox = document.createElement('input')
     newCheckbox.type = 'checkbox'
     newCheckbox.id = 'task-check'
+
     let newListObj = document.createElement('li')
     newListObj.id = 'task'
     
     let newText = document.createTextNode(newListText.value)
 
-    let listId = document.getElementById('task-list')
-
     newListObj.appendChild(newCheckbox)
     newListObj.appendChild(newText)
-    
     listId.appendChild(newListObj)
-    
+
+    // Local storage
+    todosArr.push(newListObj.innerHTML)
+    localStorage.setItem("todos", JSON.stringify(todosArr))
 
     newListText.value = null
 }
 
 window.onload=function()
 {
-    
-    let buttonId = document.getElementById('create-task-button')
-    let newListText = document.getElementById('create-task-text')
-    buttonId.addEventListener('click', OnClickTaskButton)
-    
-    newListText.addEventListener('keypress', function(e)
+    listId.innerHTML = ''
+
+    if(todos)
     {
-        if (e.key === "Enter") 
-            OnClickTaskButton();          
-    })
-    
+        for(let i = 0; i < todos.length; i++)
+        {      
+            listId.innerHTML += `${todos[i]}`
+        }
+    }
 }
-
-
